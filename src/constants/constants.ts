191236fbe9498ad5d38ea66d0fa66b4ca016274e
nobelium24 <ogunbaja07@gmail.com>
2025-08-10 09:30:11 +0100

    minor update

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,5 @@
-// export const ROOT_URL = "https://realtimefileeditor.onrender.com"
+export const ROOT_URL = "https://realtimefileeditor.onrender.com"
 
-export const ROOT_URL = "http://localhost:9091";
+// export const ROOT_URL = "http://localhost:9091";
+
+// export const ROOT_URL = process.env.ROOT_URL as string;
